4175922d8454e323af0b29234368a16a540b9519
Swatts96 <w0292233@nscc.ca>
2025-02-07 03:00:37 -0400

    added discussion model

--- HelloMVC/Models/Discussion.cs
+++ HelloMVC/Models/Discussion.cs
@@ -1,6 +1,21 @@
-﻿namespace HelloMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using HelloMVC.Models;
+
+namespace HelloMVC.Models
 {
     public class Discussion
     {
+        // Primary key
+        public int DiscussionId { get; set; }
+
+        // Properties
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageFilename { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        // One-to-many relationship with Comment
+        public ICollection<Comment> Comments { get; set; }
     }
 }
