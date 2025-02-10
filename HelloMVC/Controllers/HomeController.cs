c924af6f3731c0c561328ee84ac1bf472d4d1425
Swatts96 <w0292233@nscc.ca>
2025-02-09 22:10:11 -0400

    working on home and discussion pages. re-arranging

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -1,88 +1,90 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using HelloMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using HelloMVC.Data;
+using HelloMVC.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HelloMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-            // Constructor logic (if needed)
+        private readonly HelloMVCContext _context;
 
+        public HomeController(HelloMVCContext context)
+        {
+            _context = context;
         }
 
-        // Home page ---- Display all discussions
-        public IActionResult Index()
+        // GET: Home/Index
+        public async Task<IActionResult> Index()
         {
-            // Create a list of discussions
-            List<Discussion> discussions = new List<Discussion>();
-
-            // Create 3 discussion objects 
-            Discussion discussion1 = new Discussion()
-            {
-                DiscussionId = 1,
-                Title = "Sidney Crosby Injured before four nations tournament",
-                Content = "He was injured and now he can't play",
-                ImageFilename = "crosby.jpg",
-                IsPublic = true, // Ensure this property exists in your Discussion model
-                Comments = new List<Comment>() // Each discussion has comments
-            };
-
-            Discussion discussion2 = new Discussion()
-            {
-                DiscussionId = 2,
-                Title = "Connor Mcdavid traded to Minnesota??",
-                Content = "He was injured and now he can't play",
-                ImageFilename = "crosby.jpg",
-                IsPublic = true,
-                Comments = new List<Comment>() // Each discussion has comments
-            };
-
-            Discussion discussion3 = new Discussion()
-            {
-                DiscussionId = 3, // Corrected ID
-                Title = "Malkin playing",
-                Content = "He was injured and now he can't play",
-                ImageFilename = "malkin.jpg",
-                IsPublic = true,
-                Comments = new List<Comment>() // Each discussion has comments
-            };
-
-            // Add to the list
-            discussions.Add(discussion1);
-            discussions.Add(discussion2);
-            discussions.Add(discussion3);
-
-            // Pass the list to the view
+            // Load discussions, including comments, ordered descending by CreateDate
+            var discussions = await _context.Discussion
+                .Include(d => d.Comments)
+                .OrderByDescending(d => d.CreateDate)
+                .ToListAsync();
             return View(discussions);
         }
 
-
-
-        // Display discussion by id 
-        public IActionResult DisplayDiscussion(int id)
+        // GET: Home/GetDiscussion/5
+        public async Task<IActionResult> GetDiscussion(int id)
         {
-
-            //To do : Entity framework 
-            Discussion discussion = new Discussion()
+            // Load a single discussion (with its comments)
+            var discussion = await _context.Discussion
+                .Include(d => d.Comments)
+                .FirstOrDefaultAsync(d => d.DiscussionId == id);
+            if (discussion == null)
             {
-                DiscussionId = id,
-                Title = "Ovechkin",
-                Content = "good player",
-                ImageFilename = "ovechkin.jpg",
-                IsPublic = true,
-                Comments = new List<Comment>() // Each discussion has comments
-            };
+                return NotFound();
+            }
+            // Optionally order the comments descending
+            discussion.Comments = discussion.Comments.OrderByDescending(c => c.CreateDate).ToList();
             return View(discussion);
         }
 
-
-        // Privacy page 
         public IActionResult Privacy()
         {
             return View();
         }
     }
 }
+
+//// Create 3 discussion objects 
+//Discussion discussion1 = new Discussion()
+//            {
+//                DiscussionId = 1,
+//                Title = "Sidney Crosby Injured before four nations tournament",
+//                Content = "He was injured and now he can't play",
+//                ImageFilename = "crosby.jpg",
+//                IsPublic = true, // Ensure this property exists in your Discussion model
+//                Comments = new List<Comment>() // Each discussion has comments
+//            };
+
+//            Discussion discussion2 = new Discussion()
+//            {
+//                DiscussionId = 2,
+//                Title = "Connor Mcdavid traded to Minnesota??",
+//                Content = "He was injured and now he can't play",
+//                ImageFilename = "crosby.jpg",
+//                IsPublic = true,
+//                Comments = new List<Comment>() // Each discussion has comments
+//            };
+
+//            Discussion discussion3 = new Discussion()
+//            {
+//                DiscussionId = 3, // Corrected ID
+//                Title = "Malkin playing",
+//                Content = "He was injured and now he can't play",
+//                ImageFilename = "malkin.jpg",
+//                IsPublic = true,
+//                Comments = new List<Comment>() // Each discussion has comments
+//            };
+
+//            // Add to the list
+//            discussions.Add(discussion1);
+//            discussions.Add(discussion2);
+//            discussions.Add(discussion3);
+
+//            // Pass the list to the view
+//            return View(discussions)
